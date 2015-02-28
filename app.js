9fce543de3603316c433f10b8ada43e8dbbfe285
Nicolas Marchildon <nicolas@marchildon.net>
2015-02-28 12:58:07 -0500

    Read from DB, capacity in gallons, and arc charts: * Serve TSV file from Node app, read from SQLite DB. * Remove CSV file. * Compute tank level in gallons. * Decrease sampling delay to 10 seconds. * Display arc charts instead of line chart.

--- app.js
+++ app.js
@@ -17,10 +17,32 @@ app.use(app.router);
 app.use(express.logger());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', express.static(path.join(__dirname, 'index.html')));
+app.get('/tank/:name/levels.tsv', function(req, res) {
+  console.log('Get ' + req.params.name);
+  db.serialize(function() {
+    db.all("select reading_date, gallons from tank_reading where device_name = ? and datetime(reading_date / 1000, 'unixepoch') >= date('now','-1 day') order by reading_date desc", [req.params.name], function(err, rows) {
+      if (err) {
+        console.log(err);
+        return res.send(500, err);
+      }
+      var tsvRows = rows.map(function(row) { return [row.reading_date, row.gallons].join('\t') });
+      var tsv = ['reading_date\tgallons'].concat(tsvRows);
+      //console.log(tsv.join(', '));
+      res.setHeader("Content-Type", "text/plain");
+      res.send(tsv.join('\n'));
+    });
+  });
+});
 
 var tankIds = [
   process.env.TANK_1_DEVICE_ID,
   process.env.TANK_2_DEVICE_ID];
+
+var tanks = {};
+tanks[tankIds[0]] = { name:"JR1", tubeLength: 1847, height:1650, capacity:6500, type: "rond65"};
+tanks[tankIds[1]] = { name:"JR2", tubeLength: 1375, height:1079, capacity: 900, type: "u42.5"};
+
+var sampleSeconds = 10;
 
 spark.login({accessToken: accessToken}).then(
   function(token){
@@ -34,7 +56,7 @@ spark.login({accessToken: accessToken}).then(
         tanks.forEach(function(tank) {
           update(tank);
         });
-        setTimeout(queryDevices, 5000);
+        setTimeout(queryDevices, sampleSeconds * 1000);
       }
       queryDevices();
     });
@@ -51,8 +73,7 @@ function update(device) {
       //console.log('Got result:', data);
       var deviceID = data.coreInfo.deviceID;
       console.log(device.name + "." + variableName + ": " + data.result);
-      insertTankReading(deviceID, device.name, data.result, -1);
-      appendTankReadingCSV(deviceID, device.name, data.result, -1);
+      insertTankReading(deviceID, device.name, data.result, raw2gallons(data.result, deviceID));
     },
     function(err) {
       console.log('An error occurred while getting attrs:', err);
@@ -60,19 +81,18 @@ function update(device) {
   );
 }
 
+function raw2gallons(raw, tankId) {
+  var tank = tanks[tankId];
+  console.log("raw2gallons: " + raw + ", " + tankId + ", " + tank);
+  var sampleHeight = tank.tubeLength - raw;
+  var fillFactor = sampleHeight / tank.height;
+  return tank.capacity * fillFactor;
+}
+
 function insertTankReading(deviceID, deviceName, rawReading, gallons) {
   db.serialize(function() {
     db.run("INSERT INTO tank_reading (device_id, device_name, reading_date, raw_reading, gallons) VALUES (?, ?, ?, ?, ?)",
-        [ deviceID, deviceName, new Date(), rawReading, gallons ]);
-  });
-}
-
-function appendTankReadingCSV(deviceID, deviceName, rawReading, gallons) {
-  var timestamp = new Date().getTime();
-  fs.appendFile(tankLevelFile, ["" + timestamp, deviceName, rawReading, gallons].join("\t") + "\n", function(err) {
-    if (err) {
-      console.log(err);
-    }
+        [ deviceID, deviceName, new Date(), rawReading, gallons.toFixed(0) ]);
   });
 }
 
