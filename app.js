57f4e2a4c4d835f2a331603e8f6793cc2055441f
Pierre Leboeuf <pierre1.leboeuf@gmail.com>
2023-02-08 15:05:25 -0500

    Fixed use of dotenv
    - Completed the modification to use the environment variable to store login info.
    - Replaced sone var statement by const. Best practice
    - added the dotenv package

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-const fs = require('fs');
-const Promise = require('promise');
+require("dotenv").config();
+const fs = require("fs");
+const Promise = require("promise");
 const readFile = Promise.denodeify(fs.readFile);
-const http = require('http');
-const Particle = require('particle-api-js');
-const sqlite3 = require('better-sqlite3');
-const express = require('express');
-const path = require('path');
-const chalk = require('chalk');
+const http = require("http");
+const Particle = require("particle-api-js");
+const sqlite3 = require("better-sqlite3");
+const express = require("express");
+const path = require("path");
+const chalk = require("chalk");
 // const Watchout = require('watchout')
-const Command = require('./command.js');
-const WebSocket = require('ws');
-
-const eventmodule = require('./event.js');
-var config = require('./config.json');
-var dbFile = config.database || 'raw_events.sqlite3';
-var particle = new Particle();
-var password = config.password;
-var username = config.username;
+const Command = require("./command.js");
+const WebSocket = require("ws");
+const eventmodule = require("./event.js");
+const config = require("./config.json");
+const dbFile = config.database || "raw_events.sqlite3";
+const particle = new Particle();
+const password = process.env.PARTICLE_TOKEN;
+const username = process.env.PARTICLE_USER;
 var accessToken;
 var eventDB;
 const nodeArg = process.argv;
@@ -28,18 +28,17 @@ function devString(deviceId) {
 function startApp(db) {
     console.log(chalk.gray("Starting application..."));
     eventDB = new eventmodule.EventDatabase(db);
-    var app = createExpressApp(db);
+    const app = createExpressApp(db);
     // Do not connect to Particle cloud for playbackOnly
-    
-    connectToParticleCloud(db, eventDB, nodeArg[2], nodeArg[3])
+    connectToParticleCloud(db, eventDB, nodeArg[2], nodeArg[3]);
 
     try {
-        var port = config.port || '3000';
-        app.set('port', port);
-        var server = http.createServer(app);
+        const port = config.port || "3000";
+        app.set("port", port);
+        const server = http.createServer(app);
         createWebSocketServer(server, eventDB);
         server.listen(port);
-        console.log(chalk.green('Server started: http://localhost:%s'), port);
+        console.log(chalk.green("Server started: http://localhost:%s"), port);
     } catch (err) {
         console.error(err);
         throw err;
@@ -48,10 +47,12 @@ function startApp(db) {
 
 function ensureDatabase() {
     return new Promise(function (resolve, reject) {
-        fs.open(dbFile, 'r', function (err, fd) {
+        fs.open(dbFile, "r", function (err, fd) {
             if (err) {
                 console.log(chalk.gray("Creating database: %s"), dbFile);
-                readFile('schema.sql', 'utf8').then(createDatabase).then(resolve, reject);
+                readFile("schema.sql", "utf8")
+                    .then(createDatabase)
+                    .then(resolve, reject);
             } else {
                 console.log(chalk.gray("Using existing database: %s"), dbFile);
                 resolve(new sqlite3(dbFile));
@@ -76,21 +77,22 @@ function createExpressApp() {
     var app = express();
     // app.use(app.router);
     // app.use(express.logger());
-    app.use(express.static(path.join(__dirname, 'public')));
-    app.use('/', express.static(path.join(__dirname, 'index.html')));
-    app.get('/device/:id', function (req, res) {
+    app.use(express.static(path.join(__dirname, "public")));
+    app.use("/", express.static(path.join(__dirname, "index.html")));
+    app.get("/device/:id", function (req, res) {
         var generationId = req.query.generation;
         var serialNo = req.query.since;
-        var sql = "select * from raw_events where device_id = ? and generation_id = ? and serial_no > ?";
+        var sql =
+            "select * from raw_events where device_id = ? and generation_id = ? and serial_no > ?";
         var params = [req.params.id, generationId, serialNo];
         try {
             const rows = db.prepare(sql).all(params);
             var events = rows.map(function (row) {
                 return {
-                    "coreid": row.device_id,
-                    "published_at": row.published_at,
-                    "name": "collector/replay",
-                    "data": row.raw_data
+                    coreid: row.device_id,
+                    published_at: row.published_at,
+                    name: "collector/replay",
+                    data: row.raw_data,
                 };
             });
             res.setHeader("Content-Type", "text/plain");
@@ -104,71 +106,95 @@ function createExpressApp() {
 }
 
 function connectToParticleCloud(db, eventDB, streamOption, replayOption) {
-    particle.login({
-        username, password
-    }).then(
-        function (data) {
-            accessToken = data.body.access_token;
-            console.log(chalk.gray('Login to cloud completed. Listing devices...'));
-            particle.listDevices({ auth: accessToken }).then(
-                function (Devices) {
-                    const myDevices = Devices.body;
-                    console.log(chalk.gray('Got %s devices from cloud.'), myDevices.length);
-                    myDevices.forEach(function (dev) {
-                        eventDB.setAttributes(dev.id, dev);
-                    });
-                    if (replayOption === "allDeviceReplay"){
-                        requestAllDeviceReplay(db);
+    particle
+        .login({
+            username,
+            password,
+        })
+        .then(
+            function (data) {
+                accessToken = data.body.access_token;
+                console.log(
+                    chalk.gray("Login to cloud completed. Listing devices...")
+                );
+                particle.listDevices({ auth: accessToken }).then(
+                    function (Devices) {
+                        const myDevices = Devices.body;
+                        console.log(
+                            chalk.gray("Got %s devices from cloud."),
+                            myDevices.length
+                        );
+                        myDevices.forEach(function (dev) {
+                            eventDB.setAttributes(dev.id, dev);
+                        });
+                        if (replayOption === "allDeviceReplay") {
+                            requestAllDeviceReplay(db);
+                        }
+                        if (streamOption !== "noStream") {
+                            openStream(db, eventDB);
+                        }
+                    },
+                    function (err) {
+                        console.log(
+                            chalk.red("List devices call failed: %s"),
+                            err
+                        );
+                        connectToParticleCloud();
                     }
-                    if (streamOption !== "noStream"){
-                        openStream(db, eventDB);
-                    }
-                },
-                function (err) {
-                    console.log(chalk.red('List devices call failed: %s'), err);
-                    connectToParticleCloud();
-                }
-            );
-        },
-        function (err) {
-            console.log(chalk.red('Login failed: %s'), err);
-        }
-    );
+                );
+            },
+            function (err) {
+                console.log(chalk.red("Login failed: %s"), err);
+            }
+        );
 }
 
 function openStream(db, eventDB) {
-    console.log(chalk.gray('Connecting to event stream.'));
-    particle.getEventStream({ deviceId: 'mine', auth: accessToken }).then(function(stream) {
-        stream.on('event', function(event) {
-            console.log("Event: ", event);
-            if (event.code == "ETIMEDOUT") {
-                console.error(chalk.red(Date() + " Timeout error"));
-            } else {
-                eventDB.handleEvent(event);
-            }
-        });
-        }).catch(function(err) {
-            console.log(chalk.red('Stream failed: %s'), err);
+    console.log(chalk.gray("Connecting to event stream."));
+    particle
+        .getEventStream({ deviceId: "mine", auth: accessToken })
+        .then(function (stream) {
+            stream.on("event", function (event) {
+                console.log("Event: ", event);
+                if (event.code == "ETIMEDOUT") {
+                    console.error(chalk.red(Date() + " Timeout error"));
+                } else {
+                    eventDB.handleEvent(event);
+                }
+            });
+        })
+        .catch(function (err) {
+            console.log(chalk.red("Stream failed: %s"), err);
         });
 }
 
 function requestAllDeviceReplay(db) {
-    var sql = "select raw_events.device_id as device_id, raw_events.generation_id as generation_id, max(raw_events.serial_no) as serial_no from raw_events, (select device_id, max(generation_id) as generation_id from raw_events group by device_id) as gens where raw_events.device_id = gens.device_id and raw_events.generation_id = gens.generation_id group by raw_events.device_id";
-    console.log(chalk.blue('Requesting playback!!!'));
+    var sql =
+        "select raw_events.device_id as device_id, raw_events.generation_id as generation_id, max(raw_events.serial_no) as serial_no from raw_events, (select device_id, max(generation_id) as generation_id from raw_events group by device_id) as gens where raw_events.device_id = gens.device_id and raw_events.generation_id = gens.generation_id group by raw_events.device_id";
+    console.log(chalk.blue("Requesting playback!!!"));
     for (const row of db.prepare(sql).iterate()) {
         try {
             if (row.device_id == null) {
                 // Ignoring empty row returned by sqlite aggregate function on empty result.
                 console.log(chalk.gray("No devices to request a replay from."));
             } else if (typeof row.generation_id === "undefined") {
-                console.error(chalk.red("Got undefined generation for device %s. Don't know what to request. Waiting for new events. POSSIBLE DATA LOSS!"), row.device_id);
+                console.error(
+                    chalk.red(
+                        "Got undefined generation for device %s. Don't know what to request. Waiting for new events. POSSIBLE DATA LOSS!"
+                    ),
+                    row.device_id
+                );
             } else {
-                requestDeviceReplay(row.device_id, row.generation_id, row.serial_no);
-            }           
+                requestDeviceReplay(
+                    row.device_id,
+                    row.generation_id,
+                    row.serial_no
+                );
+            }
         } catch (err) {
             throw err;
         }
-    };
+    }
 }
 
 // function requestAllSeasonReplay(db) {
@@ -183,7 +209,7 @@ function requestAllDeviceReplay(db) {
 //                 console.error(chalk.red("Got undefined generation for device %s. Don't know what to request. Waiting for new events. POSSIBLE DATA LOSS!"), row.device_id);
 //             } else {
 //                 requestDeviceReplay(row.device_id, row.generation_id, row.serial_no);
-//             }           
+//             }
 //         } catch (err) {
 //             throw err;
 //         }
@@ -191,52 +217,97 @@ function requestAllDeviceReplay(db) {
 // }
 
 function requestDeviceReplay(deviceId, generationId, serialNo) {
-    console.log(chalk.gray("Requesting replay on %s at %s,%s"), devString(deviceId), generationId, serialNo);
+    console.log(
+        chalk.gray("Requesting replay on %s at %s,%s"),
+        devString(deviceId),
+        generationId,
+        serialNo
+    );
     particle.getDevice(deviceId, function (err, device) {
-
         if (device != null) {
-            device.callFunction('replay', "" + serialNo + ", " + generationId, function (err, data) {
-                // Return codes:
-                //   0  Success
-                //  -1  Failure
-                //  -2  A replay is already in progress
-                // -99  Invalid generation ID
-                if (err) {
-                    console.error(chalk.red("Replay request failed: '%s' on %s at %s,%s with data: %s. EVENTS MAY BE LOST! Ensure the device is online, then restart the collector to request a new replay."), err, devString(deviceId), generationId, serialNo, data);
-                } else {
-                    if (data.return_value == 0) {
-                        console.log(chalk.green('Replay request on %s successful.'), devString(deviceId));
+            device.callFunction(
+                "replay",
+                "" + serialNo + ", " + generationId,
+                function (err, data) {
+                    // Return codes:
+                    //   0  Success
+                    //  -1  Failure
+                    //  -2  A replay is already in progress
+                    // -99  Invalid generation ID
+                    if (err) {
+                        console.error(
+                            chalk.red(
+                                "Replay request failed: '%s' on %s at %s,%s with data: %s. EVENTS MAY BE LOST! Ensure the device is online, then restart the collector to request a new replay."
+                            ),
+                            err,
+                            devString(deviceId),
+                            generationId,
+                            serialNo,
+                            data
+                        );
                     } else {
-                        console.log(chalk.yellow('Replay request refused by %s at %s,%s with code %s. EVENTS MAY BE LOST! Waiting for events.'), devString(deviceId), generationId, serialNo, data.return_value);
+                        if (data.return_value == 0) {
+                            console.log(
+                                chalk.green("Replay request on %s successful."),
+                                devString(deviceId)
+                            );
+                        } else {
+                            console.log(
+                                chalk.yellow(
+                                    "Replay request refused by %s at %s,%s with code %s. EVENTS MAY BE LOST! Waiting for events."
+                                ),
+                                devString(deviceId),
+                                generationId,
+                                serialNo,
+                                data.return_value
+                            );
+                        }
                     }
                 }
-            });
+            );
         }
-
     });
 }
 
 function createWebSocketServer(server, eventDB, commandHandler) {
-    const wss = new WebSocket.Server({server: server});
+    const wss = new WebSocket.Server({ server: server });
     const connectedClients = [];
-    wss.on('connection', function connection(ws, req) {
+    wss.on("connection", function connection(ws, req) {
         connectedClients.push(ws);
-        console.log(chalk.gray((new Date()) + ' Connection accepted. Connections: ' + connectedClients.length), req.socket.remoteAddress);
-        ws.on('message', function incoming(message) {
-            const db = new sqlite3(dbFile, {readonly: true});
+        console.log(
+            chalk.gray(
+                new Date() +
+                    " Connection accepted. Connections: " +
+                    connectedClients.length
+            ),
+            req.socket.remoteAddress
+        );
+        ws.on("message", function incoming(message) {
+            const db = new sqlite3(dbFile, { readonly: true });
             try {
-                console.log(chalk.gray('Received Message: %s'), message);
+                console.log(chalk.gray("Received Message: %s"), message);
                 var command = JSON.parse(message);
-                const commandHandler = Command.CommandHandler(db, config.blacklist);
+                const commandHandler = Command.CommandHandler(
+                    db,
+                    config.blacklist
+                );
                 commandHandler.onCommand(command, ws);
             } catch (exception) {
                 console.error(chalk.red(exception));
                 db.close();
             }
         });
-        ws.on('close', function (reasonCode, description) {
+        ws.on("close", function (reasonCode, description) {
             connectedClients.splice(connectedClients.indexOf(connection), 1);
-            console.log(chalk.gray((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected. Connections: ' + connectedClients.length));
+            console.log(
+                chalk.gray(
+                    new Date() +
+                        " Peer " +
+                        connection.remoteAddress +
+                        " disconnected. Connections: " +
+                        connectedClients.length
+                )
+            );
         });
     });
 
@@ -249,6 +320,8 @@ function createWebSocketServer(server, eventDB, commandHandler) {
     });
 }
 
-ensureDatabase().then(startApp).catch(function (err) {
-    console.error(chalk.red(err));
-});
+ensureDatabase()
+    .then(startApp)
+    .catch(function (err) {
+        console.error(chalk.red(err));
+    });
